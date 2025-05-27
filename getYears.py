eb81a521082327afd48a5fcb151e2dbd98f76d66
User <Willspri@gmail.com>
2025-05-27 14:19:17 +1200

    update

--- getYears.py
+++ getYears.py
@@ -1,10 +1,10 @@
-
 import calendar 
 
 weekDay = input("Enter weekday: ")
-date = input("Enter day of month: ")
+date = input("Enter day of month:  ")
 month, day = map(int, date.split())
 
+# Map weekday name to number
 weekday_map = {
     'Monday': 0,
     'Tuesday': 1,
@@ -13,10 +13,16 @@ weekday_map = {
     'Friday': 4,
     'Saturday': 5,
     'Sunday': 6
-    }
+}
 
-target_day = weekday_map.get(weekDay)
+# Get numeric weekday
+target_day = weekday_map.get(weekDay.capitalize())
 
+if target_day is None:
+    print("Invalid weekday entered.")
+    exit()
+
+# Find matching years
 matching_years = []
 for year in range(1900, 2101):
     try:
@@ -24,7 +30,10 @@ for year in range(1900, 2101):
             matching_years.append(year)
     except ValueError:
         continue
-    print(matching_years)
+
+# Print result
+print(f"\nYears when {month}/{day} fell on a {weekDay.capitalize()}:")
+print(matching_years)
 #print(calendar.year(weekDay, date))
 #for i in years 
 #return years
